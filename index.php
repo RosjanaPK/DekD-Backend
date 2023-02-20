ff51319dad123ab2eb1874c9ef3bc7f2e1a890f8
RosjanaPK <rajna471@gmail.com>
2023-02-21 00:43:32 +0700

    update

--- index.php
+++ index.php
@@ -4,14 +4,106 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <title>Document</title>
 </head>
+<style>
+    .body {
+        background-color: white;
+        display: inline-block;
+        width: 100%;
+    }
+
+    .content {
+        display: inline;
+        text-align: center;
+
+    }
+
+    .head-text {
+        color: #F85A18;
+        font-size: 2vw;
+        font-weight: bold;
+        justify-items: center;
+        justify-content: center;
+    }
+
+    .mid-content {
+        width: 100%;
+        display: inline-block;
+    }
+
+    .middle,
+    .top {
+        display: inline-block;
+        left: 50%;
+        width: 50%;
+        text-align: left;
+
+    }
+
+    .middle {
+        border-style: outset;
+
+    }
+</style>
 
 <body>
-    <?php
-    echo "My first PHP script!";
-    ?>
+    <div class="body">
+        <div class="content">
+            <p class="head-text">ตั้งกระทู้ใหม่</p>
+            <div class="mid-content">
+                <div class="top">
+                    <div style="display: inline-block; border-right: double; padding-right: 5vw; color:#A69481; padding-left: 5vw; ">
+                        <i style='font-size:24px; color:#A69481' class='fas'>&#xf304;</i>
+                        <p style='color:#A69481;'> ปกติ</p>
+                    </div>
+                    <div style="display: inline-block; border-right: double; padding-right: 5vw; padding-left: 5vw; color:#A69481">
+                        <i style='font-size:24px; color:#A69481' class='fas'>&#xf03e;</i>
+                        <p style='color:#A69481'>รูปภาพ</p>
+                    </div>
+                    <div style="display: inline-block; border-right: double; padding-right: 5vw; padding-left: 5vw; color:#A69481">
+                        <i style='font-size:24px; color:#A69481' class='fas'>&#xf201;</i>
+                        <p style='color:#A69481'>โพลล์</p>
+                    </div>
+                    <div style="display: inline-block; padding-left: 5vw; color:#A69481">
+                        <i style='font-size:24px; color:#A69481' class='fas'>&#xf086;</i>
+                        <p style='color:#A69481'>โต้วาที</p>
+                    </div>
+                </div>
+                <div class="middle">
+                    <div>
+                        <p>หัวข้อกระทู้</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p style="display: inline; ">เนื้อหากระทู้</p>
+                            <p style="display: inline; padding-left: 80%;">20000</p>
+                        </div>
 
+                        <div style="display: block;">
+
+                            <p style=" display: inline; border-style:inset;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086;</i> Source</p>
+                            <p style=" display: inline; border-style:inset; padding-left: 1vw;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; &#xf086; &#xf086; &#xf086; &#xf086; &#xf086; &#xf086;</i></p>
+                            <p style=" display: inline; border-style:inset; padding-left: 1vw;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; &#xf086; &#xf086; &#xf086; &#xf086;</i></p>
+
+                        </div>
+                        <div style="display: block;">
+                                
+                                <p style=" display: inline; border-style:inset;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; &#xf086; &#xf086; &#xf086;</i></p>
+                                <p style=" display: inline; border-style:inset;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; &#xf086; &#xf086; &#xf086;</i></p>
+                                <p style=" display: inline; border-style:inset;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; &#xf086; &#xf086;</i></p>
+                                <p style=" display: inline; border-style:inset;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; </i></p>
+                                <p style=" display: inline; border-style:inset;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; &#xf086;</i></p>
+                                <p style=" display: inline; border-style:inset;"><i style='font-size:16px; color:#A69481' class='fas'>&#xf086; </i></p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </body>
 
 </html>
